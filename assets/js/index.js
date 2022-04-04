f432d1f2da9367968acc3bc286da61c937843be3
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-04-04 18:49:32 +0300

    blog post assignment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,23 @@
 // accessing elements on the page
 //access using css class
-let buttons = document.getElementsByClassName('btn');
-console.log(buttons)
-
+let buttons = document.getElementsByClassName("btn");
+console.log(buttons);
 
 // access elements using element id
-let byId = document.getElementById('second');
-let byId2 = document.getElementById('seconds'); //  this fails since we have no ID of seconds
-
+let byId = document.getElementById("second");
+let byId2 = document.getElementById("seconds"); //  this fails since we have no ID of seconds
 
 // access by tagName
-let allH3s = document.getElementsByTagName('h3');
+let allH3s = document.getElementsByTagName("h3");
 // console.log(allH3s)
 
 // single element querySelector
 console.log(document.querySelector("#second"));
-console.log('first btn ', document.querySelector(".btn"));
-
+console.log("first btn ", document.querySelector(".btn"));
 
 // all items using querySelectorAll
-console.log('every btn', document.querySelectorAll(".btn"));
+console.log("every btn", document.querySelectorAll(".btn"));
 let inputElem = document.querySelector("input[type='email']");
-
-// blogDatabase
-const posts = [];
 
 // simple form processing
 const form = document.querySelector("form");
@@ -33,21 +27,26 @@ let title = form.title;
 
 // process email and get author
 function getAuthor(email) {
-    return email ? email.substring(0, email.lastIndexOf('@')): '';
+  return email ? email.substring(0, email.lastIndexOf("@")) : "";
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();  // stop operations until i say so
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // stop operations until i say so
+  // blogDatabase
+  const posts = [
+    {
+      author: getAuthor(email.value),
+      title: title.value,
+      content: content.value,
+    },
+  ];
+  console.log("contents of database", posts);
 
-
-    let blogBost = {
-        author: getAuthor(email.value),
-        title: title.value,
-        content: content.value
-    }
-
-    // console.log('submited', blogBost)
-})
-
+  //   let blogBost = {
+  //     author: getAuthor(email.value),
+  //     title: title.value,
+  //     content: content.value,
+  //   };
+});
 
 // console.log(getAuthor('armstrongsouljah@gmail.com'))
